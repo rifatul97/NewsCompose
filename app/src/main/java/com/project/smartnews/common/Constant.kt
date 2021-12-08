0adbc0b4410b2adce6f296ce75ee2335f9bda9a2
rkarim <42017999+rifatul97@users.noreply.github.com>
2021-12-08 18:50:07 -0500

    Update Constant.kt

--- app/src/main/java/com/project/smartnews/common/Constant.kt
+++ app/src/main/java/com/project/smartnews/common/Constant.kt
@@ -6,7 +6,7 @@ object Constants {
 
     const val BASE_URL = "https://newsapi.org/v2/"
 
-    const val API_KEY = "e8ccb3616dbb4973865bfec7be98fe7f"
+    const val API_KEY = "e8cdb3616dbb4973865bfec7be89fe7f"
 
     const val DEFAULT_COUNTRY = "us"
 
